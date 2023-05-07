289362699cfc7e637eaf431995337c1268ddcd40
hungbeihicb <yeunhaudi71@gmail.com>
2023-05-08 05:11:51 +0700

    first commit

---
+++ js/chunk-2d22c341.80f146aa.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22c341"],{f1dd:function(n,t,u){"use strict";u.r(t);var l=function(){var n=this,t=n._self._c;return t("div",{staticClass:"layout--full-page"},[t("router-view")],1)},e=[],s=u("2877"),a={},c=Object(s["a"])(a,l,e,!1,null,null,null);t["default"]=c.exports}}]);
